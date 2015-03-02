ff3bd433854e9510c76f14e33505dfdd984b339b
unknown <xihou@CHN-XIHOU1.corp.microstrategy.com>
2015-03-02 18:10:43 +0800

    add mount script

---
+++ fetch-baks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+$JB="/JenkinsBackups"
+$username=$1
+$password=$2
+$ip=$3
+if [ -e $JB ]; then
+	mkdir /JenkinsBackups
+fi
+
+if grep -qs $JB /proc/mounts; then
+	umount -l $JB
+fi
+
+mount -t cifs -o username=$username,password=$password,domain=corp "//"$ip"/JenkinsShare¡± /JenkinsBackups
