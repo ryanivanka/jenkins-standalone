dbef42623fce6ec9479256b77105492860b92f2e
unknown <xihou@CHN-XIHOU1.corp.microstrategy.com>
2015-03-03 18:20:31 +0800

    add location to fetch bak

--- fetch-baks.sh
+++ fetch-baks.sh
@@ -7,25 +7,23 @@ ip=$3
 #where to backup
 location=$4
 if [ ! -e $rootPath ]; then
-	mkdir /JenkinsBackups
+        mkdir /JenkinsBackups
 fi
 
 if grep -qs $rootPath /proc/mounts; then
-	umount -l $rootPath
+        umount -l $rootPath
 fi
 mount -t cifs -o username=$username,password=$password,domain=corp "//"$ip"/JenkinsShare" /JenkinsBackups
 
 #copy the backups
 for file in $(ls "/JenkinsBackups/"$location  -t)
 do
-    if [ -d "/JenkinsBackups/"$file ];then
+    if [ -d "/JenkinsBackups/"$location/$file ];then
                 if [[ ${file:0:4} == "FULL" ]];then
-                        \cp -rf /JenkinsBackups/$file/* .
+                        echo $file
+                        \cp -rf /JenkinsBackups/$location/$file/* .
                         break
                 fi
     fi
 done
 echo "restore is done..."
-
-
-        
