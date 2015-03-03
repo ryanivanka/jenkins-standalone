f14ce9a034b4917b34ec7b00de6d6edebb5cda6a
unknown <xihou@CHN-XIHOU1.corp.microstrategy.com>
2015-03-03 16:54:53 +0800

    add update master url info

--- Customize.py
+++ Customize.py
@@ -1,12 +1,19 @@
 #Change Jenkins master url for new APP
-
-
-#Read APP info first.
-import xml.etree.ElementTree as ET
+#Update backup location
+#import xml.etree.ElementTree as ET
+from lxml import etree as ET
 import os
 import sys
+import paramiko
 
-path=sys.argv[1]
+rootPath=sys.argv[1]
+app=sys.argv[2]
+
+HOST="10.27.16.167"
+USERNAME="root"
+PASSWD="mstr123"
+PORT="22"
+
 #change backup location for new App
 def modifyBackupPath(path):
     tree=ET.parse("thinBackup.xml")
@@ -15,12 +22,49 @@ def modifyBackupPath(path):
     print "backup path is: "+backupPath.text
 
     backupPath.text=path
-    tree.write('tmp.xml')
+    tree.write('tmp.xml', encoding='utf-8', xml_declaration=True)
     os.remove('thinBackup.xml')
     os.rename('tmp.xml', 'thinBackup.xml')
 
+# notify to update ha proxy and get port info.
+def notifyUpdateHaproxy():
+    try:
+        ssh=paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=HOST, username=USERNAME, password=PASSWD, port=PORT)
+        stdin, stdout, stderr = ssh.exec_command('/data/haproxy/haproxy_marathon_bridge logged refresh_system_haproxy localhost:8080')
+        stdin,stdout,sterr=ssh.exec_command('cat /etc/haproxy/haproxy.cfg')
+
+    #    appPort=10000
+        outs=stdout.readlines()
+        for line in outs:
+            line=line.strip()
+            if line.find("listen "+app+"-" ) > -1:
+                print line
+                ports=line.split('-')
+                appPort=ports[1]
+                break
+        ssh.close()
+        return appPort
+    except:
+        print stderr
+
+#update master url info 
+def modifyMasterURL(HOST, appPort):
+    tree=ET.parse("jenkins.model.JenkinsLocationConfiguration.xml")
+    root=tree.getroot()
+    jenkinsURL=root.find('jenkinsUrl')
+    print "jenkins URL is :"+ jenkinsURL.text
+
+    jenkinsURL.text="http://"+HOST+":"+appPort+"/"
+    tree.write('tmp.xml', encoding='utf-8', xml_declaration=True)
+    os.remove('jenkins.model.JenkinsLocationConfiguration.xml')
+    os.rename('tmp.xml','jenkins.model.JenkinsLocationConfiguration.xml')
+
 def main():
-    modifyBackupPath(path)
+    modifyBackupPath("%s/%s" %(rootPath,app))
+    appPort= notifyUpdateHaproxy()
+    modifyMasterURL(HOST, appPort)
 
 if __name__=='__main__':
     main()
